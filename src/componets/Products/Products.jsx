bc0a02014d5af4b436ae8b08f5400f8d1c2f26d7
Your Name <you@example.com>
2023-06-27 10:37:47 -0300

    Atualização dos cards

--- src/componets/Products/Products.jsx
+++ src/componets/Products/Products.jsx
@@ -10,14 +10,10 @@ function Products() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-
     fetchProducts('iphone').then((response) => {
-
       setProducts(response);
-
     });
   }, []);
-
   return ( 
     <section className="products container">
       {
